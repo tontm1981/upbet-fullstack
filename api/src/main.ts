5fec36703764ae99549c1603a170102301be12d5
Elton Moreno <tontm1981@gmail.com>
2024-06-14 11:07:17 -0300

    chore: configurando CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use((request: Request, response: Response, next: NextFunction) => {
+    response.setHeader('Access-Control-Allor-Origin', 'http://localhost:8080');
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
